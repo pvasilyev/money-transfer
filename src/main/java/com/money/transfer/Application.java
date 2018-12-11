b625019d26c55753b92012721a7167a634955cc3
pvasilyev <p_vasiliev@mail.ru>
2018-12-11 03:35:30 +0300

    Rename bank controller to be more like bank-resource; Enhance project with self-descriptive Swagger & Swagger UI which should really help in testing out RESTful API and demo it; Support corresponding servlet initializations in main application.

--- src/main/java/com/money/transfer/Application.java
+++ src/main/java/com/money/transfer/Application.java
@@ -1,27 +1,38 @@
 package com.money.transfer;
 
-import com.money.transfer.api.BankController;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/money-transfer");
-
         Server jettyServer = new Server(8080);
-        jettyServer.setHandler(context);
+        ServletContextHandler context =
+                new ServletContextHandler(jettyServer, "/money-transfer", ServletContextHandler.SESSIONS);
 
-        ServletHolder jerseyServlet = context.addServlet(
-                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+        ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/api/*");
         jerseyServlet.setInitOrder(0);
 
         // Tells the Jersey Servlet which REST service/class to load.
         jerseyServlet.setInitParameter(
-                "jersey.config.server.provider.classnames",
-                BankController.class.getCanonicalName());
+                "jersey.config.server.provider.packages",
+                "io.swagger.jaxrs.json,io.swagger.jaxrs.listing,com.money.transfer");
+
+        // Setup Swagger servlet
+        ServletHolder swaggerServlet = context.addServlet(io.swagger.jersey.config.JerseyJaxrsConfig.class, "/swagger-core");
+        swaggerServlet.setInitOrder(2);
+        swaggerServlet.setInitParameter("api.version", "1.0.0");
+        swaggerServlet.setInitParameter("swagger.api.basepath", "/money-transfer/api");
+        swaggerServlet.setInitParameter("swagger.api.host", "localhost:8080");
+        swaggerServlet.setInitParameter("swagger.api.title", "Money Transfer API");
+
+        // Setup Swagger-UI static resources
+        String resourceBasePath = Application.class.getResource("/webapp").toExternalForm();
+        context.setWelcomeFiles(new String[] {"index.html"});
+        context.setResourceBase(resourceBasePath);
+        context.addServlet(new ServletHolder(new DefaultServlet()), "/*");
 
         try {
             jettyServer.start();
--- src/main/java/com/money/transfer/api/BankResource.java
+++ src/main/java/com/money/transfer/api/BankResource.java
@@ -2,6 +2,11 @@ package com.money.transfer.api;
 
 import com.money.transfer.api.dto.Account;
 import com.money.transfer.api.dto.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,11 +14,15 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
 import java.util.Date;
 
-@Path("/api")
-public class BankController {
+@Api(value = "/", tags = {"Bank Resource"})
+@Path("/")
+public class BankResource {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @XmlRootElement
     public enum Status {
@@ -23,28 +32,32 @@ public class BankController {
     @GET
     @Path("status")
     @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Status", notes = "Simple end-point to check if Bank Service is alive.", response = Status.class)
     public Status status() {
+        LOG.info("Bank service is alive.");
         return Status.OK;
     }
 
     @GET
     @Path("user/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public User user(@PathParam("id") String userId) {
+    @ApiOperation(value = "Get user", notes = "Get user by id", response = User.class)
+    public User user(@ApiParam(value = "user id", required = true) @PathParam("id") String userId) {
         // todo make me more dynamic
         User user = new User();
         user.setId(userId);
         user.setFirstName("Peter");
         user.setLastName("Doe");
         user.setAccounts(Arrays.asList(account("random_account")));
-
+        LOG.info("Retrieving static user {}", user);
         return user;
     }
 
     @GET
     @Path("account/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Account account(@PathParam("id") String accountId) {
+    @ApiOperation(value = "Get account", notes = "Get account by id", response = User.class)
+    public Account account(@ApiParam(value = "account id", required = true) @PathParam("id") String accountId) {
         // todo make me more dynamic
         Account account = new Account();
         account.setCreated(new Date());
@@ -52,7 +65,7 @@ public class BankController {
         account.setMoney(250.75D);
         account.setStatus(Account.Status.ACTIVE);
         account.setLastModified(new Date());
-
+        LOG.info("Retrieving static account {}", account);
         return account;
     }
 
